a01aa86d06158a242b5a4d53fd923672800df71d
chrisegan623 <54693774+chrisegan623@users.noreply.github.com>
2019-09-20 14:56:13 -0400

    Create WebScraper.py

---
+++ WebScraper.py
@@ -0,0 +1,33 @@
+import requests as requests
+import time as time
+
+url  = "http://theyfightcrime.org"
+male= list()
+female = list()
+text = list()
+request = list()
+
+def my_func(url):
+        request = requests.get(url)
+        text = request.text
+        text = text.split('<P>')
+        text = (text[1])
+        text = text.split('.')
+
+        male.append(text[0])
+        female.append(text[1])
+
+if __name__ == '__main__':
+    count = 0
+    while count < 50:
+        my_func(url)
+        time.sleep(2)
+        count += 1
+
+    with open('male.txt', 'w') as m:
+        for item in male:
+            m.write("%s\n" % item)
+
+    with open('female.txt', 'w') as f:
+        for item in female:
+            f.write("%s\n" % item)
