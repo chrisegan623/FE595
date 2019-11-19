c668ddf4e62031945114e65841ae76d4bd5acbfb
chrisegan623 <54693774+chrisegan623@users.noreply.github.com>
2019-11-19 11:16:55 -0500

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.arange(0,2*np.pi,0.1)   # start,stop,step
-y = np.sin(x)
-z = np.cos(x)
 
-plt.plot(x,y,x,z)
+def graph(x,sin,cos,tan):
+    plt.plot(x,sin,x,cos,x,tan)
+    plt.legend(['sin(x)', 'cos(x)', 'tan(x)'])
+    plt.title("Sin, Cosine, and Tangent One Period")
+    plt.show()
 
-tan_y = np.tan(x)
-plt.plot(x, tan_y)
-plt.legend(['sin(x)', 'cos(x)', 'tan(x)'])
-plt.title("Sin, Cosine, and Tangent One Period")
-plt.show()
+
+if __name__ == '__main__':
+    graph(x,np.sin(x),np.cos(x),np.tan(x))
